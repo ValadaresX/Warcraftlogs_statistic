1ff443d10fee07d69ffd5383eaa767d8466fb8e4
ValadaresX <88790254+ValadaresX@users.noreply.github.com>
2021-10-18 17:01:29 -0300

    Up_date
    Adicionado função de salvar em JSON player a player.
     cirrigido erro de DataFrame index.

--- scraping.py
+++ scraping.py
@@ -46,41 +46,40 @@ while (contador <= 150):
         #Url da pagina atual
         driver.get(driver.current_url)
         print(driver.current_url[-6:].upper())        
+        contador += 1
 
         for i in character_details_links:
             driver.get(i)
             sleep(5)
+            
             
             try:
                 #Adiciona todos os itens da pagina de detalhes do player
                 df2 = pd.DataFrame({
                 "Nome": driver.find_element_by_xpath('//*[@id="character-name"]/a').text,
                 "Classe": driver.find_element_by_id('character-class').text,
-                "Item_lvl" : driver.find_element_by_id('gear-box-ilvl-text').text,
+                "Item_lvl" : driver.find_element_by_id('gear-box-ilvl-text').text[-7:],
                 "Servidor" : driver.find_element_by_xpath('//*[@id="server-link"]').text,
                 "Mortes_temporada" : driver.find_element_by_xpath('//div[2]/table/tbody/tr[2]/td[2]').text
                 },index=[0])
                 
             except:
                 print('*' * 60,'Deu erro','*' * 60)
-                sleep(randint(5,7))
                 driver.find_element_by_xpath('//*[@id="update-text"]/a').click()
+                sleep(randint(5,7))
                 continue
 
             else:
                 #Adiciona ao DataFrame os itens buscados
-                df = df.append(df2)
-                
-
+                df = df.append(df2)       
                 
             print('*' * 120)
             print(df)
-            print('*' * 120)
-    
+            print('*' * 120)   
             
-        contador += 1
-        # Gera um Json orientado a index
-        df.to_json(f"page_{contador}.json", indent=1, force_ascii=False)
+            # Gera um Json orientado a index
+            #df.to_json("C:\Users\Breno\Documents\GitHub\Warcraftlogs_statistic\Data_players.json", indent=1, orient='records', force_ascii=False)
+            
 
         #Passando para proxima pagina
         driver.get(f"https://www.warcraftlogs.com/zone/rankings/25#metric=playerscore&region=1&subregion=1&boss=-1&page={contador}")
