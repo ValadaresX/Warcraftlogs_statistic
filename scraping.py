d9e01350e22cb85ebf80c10a042d072f3a2c0763
ValadaresX <88790254+ValadaresX@users.noreply.github.com>
2021-10-19 17:27:48 -0300

    Limpeza de código

--- scraping.py
+++ scraping.py
@@ -27,64 +27,56 @@ print('Pegando o item level...')
 #Criando DataFrame
 df = pd.DataFrame(columns = ['Nome','Classe','Item_lvl','Servidor','Mortes_temporada'])
 
-
 contador = 0
-
-
 #Enquanto contador menor iqual a 150 (que nesse caso seria 15000 itens)
 while (contador <= 150):
     contador = 1
-    #Pega a variavel do 'proxima pagina'
-    next_page = driver.find_element_by_css_selector('#pagination-hook > nav > ul > li:nth-child(2) > a')
     
     #Pega todos os links dos players da pagina
     character_details_links = [href.get_attribute("href") for href in driver.find_elements_by_xpath("//td[2]/div/a[@href]")]
 
-    #Enquanto a variavel Next n for vazia
-    if next_page is not None:
-        #Url da pagina atual
-        driver.get(driver.current_url)
-        print(driver.current_url[-6:].upper())        
-        contador += 1
+    
+    #Url da pagina atual
+    driver.get(driver.current_url)
+    print(driver.current_url[-6:].upper())        
+    contador += 1
 
-        for i in character_details_links:
-            driver.get(i)
-            sleep(5)
+    for i in character_details_links:
+        driver.get(i)
+        sleep(5)
+                    
+        try:
+            #Adiciona todos os itens da pagina de detalhes do player
+            df2 = pd.DataFrame({
+            "Nome": driver.find_element_by_xpath('//*[@id="character-name"]/a').text,
+            "Classe": driver.find_element_by_id('character-class').text,
+            "Item_lvl" : driver.find_element_by_id('gear-box-ilvl-text').text[-7:],
+            "Servidor" : driver.find_element_by_xpath('//*[@id="server-link"]').text,
+            "Mortes_temporada" : driver.find_element_by_xpath('//div[2]/table/tbody/tr[2]/td[2]').text
+            },index=[0])
             
+        except:
+            print('*' * 60,'Deu erro','*' * 60)
+            driver.find_element_by_xpath('//*[@id="update-text"]/a').click()
+            sleep(randint(5,7))
+            continue
+
+        else:
+            #Adiciona ao DataFrame os itens buscados
+            #data_file = pd.read_json('Data/Data_players.json')
+            #print(data_file.Series(["Nome"]))
+            #if df2 not in data_file:
+                #df.append(df2, ignore_index=True)
+            df = df.append(df2)       
             
-            try:
-                #Adiciona todos os itens da pagina de detalhes do player
-                df2 = pd.DataFrame({
-                "Nome": driver.find_element_by_xpath('//*[@id="character-name"]/a').text,
-                "Classe": driver.find_element_by_id('character-class').text,
-                "Item_lvl" : driver.find_element_by_id('gear-box-ilvl-text').text[-7:],
-                "Servidor" : driver.find_element_by_xpath('//*[@id="server-link"]').text,
-                "Mortes_temporada" : driver.find_element_by_xpath('//div[2]/table/tbody/tr[2]/td[2]').text
-                },index=[0])
-                
-            except:
-                print('*' * 60,'Deu erro','*' * 60)
-                driver.find_element_by_xpath('//*[@id="update-text"]/a').click()
-                sleep(randint(5,7))
-                continue
+        print('*' * 120)
+        print(df)
+        print('*' * 120) 
 
-            else:
-                #Adiciona ao DataFrame os itens buscados
-                df = df.append(df2)       
-                
-            print('*' * 120)
-            print(df)
-            print('*' * 120) 
+        #df.to_json("Data/Data_players.json", indent=1, orient='records', force_ascii=False)
 
-            nome_check = driver.find_element_by_xpath('//*[@id="character-name"]/a').text
-            file_json = json.loads('Data/Data_players.json')
-
-            if df2['Nome':nome_check ] not in file_json:
-                print("Não existe")
-                df.to_json("Data/Data_players.json", indent=1, orient='records', force_ascii=False)
-
-            #Gerando json player a player
-            #df.to_json("Data/Data_players.json", indent=1, orient='records', force_ascii=False)
+        #Gerando json player a player
+        #df.to_json("Data/Data_players.json", indent=1, orient='records', force_ascii=False)
 
 
 
