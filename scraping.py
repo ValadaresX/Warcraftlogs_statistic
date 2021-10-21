c500be0e3c6130dc692bdf79e59f723c30892423
ValadaresX <88790254+ValadaresX@users.noreply.github.com>
2021-10-21 15:40:57 -0300

    Update scraping.py

--- scraping.py
+++ scraping.py
@@ -28,9 +28,9 @@ print('Pegando o item level...')
 df = pd.DataFrame(columns = ['Nome','Classe','Item_lvl','Servidor','Mortes_temporada'])
 
 contador = 0
-#Enquanto contador menor iqual a 150 (que nesse caso seria 15000 itens)
+#Enquanto contador menor igual a 150 (que nesse caso seria 15000 itens)
 while (contador <= 150):
-    contador = 1
+    
     
     #Pega todos os links dos players da pagina
     character_details_links = [href.get_attribute("href") for href in driver.find_elements_by_xpath("//td[2]/div/a[@href]")]
@@ -40,7 +40,7 @@ while (contador <= 150):
     driver.get(driver.current_url)
     print(driver.current_url[-6:].upper())        
     contador += 1
-
+    #Para cada item de player
     for i in character_details_links:
         driver.get(i)
         sleep(5)
@@ -50,24 +50,38 @@ while (contador <= 150):
             df2 = pd.DataFrame({
             "Nome": driver.find_element_by_xpath('//*[@id="character-name"]/a').text,
             "Classe": driver.find_element_by_id('character-class').text,
-            "Item_lvl" : driver.find_element_by_id('gear-box-ilvl-text').text[-7:],
+            "Item_lvl" : driver.find_element_by_id('gear-box-ilvl-text').text[-6:],
             "Servidor" : driver.find_element_by_xpath('//*[@id="server-link"]').text,
             "Mortes_temporada" : driver.find_element_by_xpath('//div[2]/table/tbody/tr[2]/td[2]').text
             },index=[0])
-            
+        
+        #Em caso de error, algumas pequenas soluções
         except:
             print('*' * 60,'Deu erro','*' * 60)
+            sleep(5)
             driver.find_element_by_xpath('//*[@id="update-text"]/a').click()
             sleep(randint(5,7))
             continue
 
         else:
             #Adiciona ao DataFrame os itens buscados
-            #data_file = pd.read_json('Data/Data_players.json')
             #print(data_file.Series(["Nome"]))
             #if df2 not in data_file:
-                #df.append(df2, ignore_index=True)
-            df = df.append(df2)       
+            #df.append(df2, ignore_index=True)
+            #df = df.append(df2)
+
+            #Abre aquivo Json que ja contem alguns itens pre-coletados       
+            data_file = pd.read_json('Data/Data_players.json')
+
+            pd.concat([data_file, df]).drop_duplicates().reset_index(drop=True)
+            for i in range(0, len(df)):
+                if df.loc[i,'Nome'] not in data_file['Nome'].unique():
+                    data_file.append(df.loc[i,::])
+            
+            
+            
+            
+            
             
         print('*' * 120)
         print(df)
