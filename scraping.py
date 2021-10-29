8eb6ed19de24d4bfc836ce4a6e82ad4b119bd0b4
ValadaresX <88790254+ValadaresX@users.noreply.github.com>
2021-10-29 18:37:56 -0300

    1.0

--- scraping.py
+++ scraping.py
@@ -26,7 +26,7 @@ print('Pegando o item level...')
 
 #Criando DataFrame
 #df = pd.DataFrame(columns = ['Nome','Classe','Item_lvl','Servidor','Mortes_temporada'])
-df3 = pd.read_json('Data/Data_players.json')
+
 
 contador = 0
 #Enquanto contador menor igual a 150 (que nesse caso coleta 15000 itens)
@@ -34,6 +34,7 @@ while (contador <= 150):
         
     #Pega todos os links dos players da pagina
     character_details_links = [href.get_attribute("href") for href in driver.find_elements_by_xpath("//td[2]/div/a[@href]")]
+    
     
     #Url da pagina atual
     driver.get(driver.current_url)
@@ -43,38 +44,38 @@ while (contador <= 150):
     for i in character_details_links:
         driver.get(i)
         sleep(5)                    
-        
+        df3 = pd.read_json('Data/Data_players.json')
         try:
-            #Adiciona todos os itens da pagina de detalhes do player
-            df2 = pd.DataFrame({
-            "Nome": [driver.find_element_by_xpath('//*[@id="character-name"]/a').text],
-            "Classe": [driver.find_element_by_id('character-class').text],
-            "Item_lvl" : [driver.find_element_by_id('gear-box-ilvl-text').text[-6:]],
-            "Servidor" : [driver.find_element_by_xpath('//*[@id="server-link"]').text],
-            "Mortes_temporada" : [driver.find_element_by_xpath('//div[2]/table/tbody/tr[2]/td[2]').text]
-            })           
-        
-        #Em caso de error, algumas pequenas soluções
+            #Adiciona todos os itens da pagina de detalhes do player:
+            nome = driver.find_element_by_xpath('//*[@id="character-name"]/a').text
+
+            #Verifica se existe o elemento nome por contagem
+            if len(df3.loc[df3['Nome'] == nome]) <= 0:
+                
+                #Captura todos os elementos no site e cria DataFrame    
+                df2 = pd.DataFrame({
+                "Nome": [driver.find_element_by_xpath('//*[@id="character-name"]/a').text],
+                "Classe": [driver.find_element_by_id('character-class').text],
+                "Item_lvl" : [driver.find_element_by_id('gear-box-ilvl-text').text[-6:]],
+                "Servidor" : [driver.find_element_by_xpath('//*[@id="server-link"]').text],
+                "Mortes_temporada" : [driver.find_element_by_xpath('//div[2]/table/tbody/tr[2]/td[2]').text]})
+
+                #Unifica os DataFrames
+                result = pd.concat([df2, df3]).drop_duplicates(subset='Nome').reset_index(drop=True)
+
+                #Cria Json ja unificado
+                result.to_json("Data/Data_players.json", indent=1, orient='records', force_ascii=False)
+                print('*' * 120)
+                print(df3)
+                print('*' * 120) 
+
+        #Em caso de error, update pagina e espere 10s
         except:
             print('*' * 60,'Deu erro','*' * 60)
             driver.find_element_by_xpath('//*[@id="update-text"]/a').click()
-            sleep(5)
+            sleep(10)
             continue
 
-        else:
-            pd.concat([df3, df2]).drop_duplicates(subset='Nome').reset_index(drop=True)            
-            
-            for i in range(0, len(df3)):
-                if df2.loc[i, 'Nome'] not in df3['Nome'].unique():
-                    df3.append(df2.loc[i,::])                  
-            
-            
-        print('*' * 120)
-        print(df3)
-        print('*' * 120) 
-
-        #Gerando json player a player
-        #df.to_json("Data/Data_players.json", indent=1, orient='records', force_ascii=False)
 
     contador += 1
     #Passando para proxima pagina
